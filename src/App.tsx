70490d540e09ebbd1ebca9557c7c4a988ff0b4b1
santosfk <santosguilhermefk@gmail.com>
2022-03-05 20:28:03 -0300

    creating the main page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import * as style from "./app.styles";
 function App() {
-  return <style.Container>..'</style.Container>;
+  return (
+    <style.Container>
+      ..
+      <style.Header>
+        <style.Title></style.Title>
+      </style.Header>
+      <style.Body></style.Body>
+    </style.Container>
+  );
 }
 
 export default App;
--- src/app.styles.ts
+++ src/app.styles.ts
@@ -3,3 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: aliceblue;
 `;
+export const Header = styled.div``;
+export const Title = styled.h1``;
+export const Body = styled.div``;
