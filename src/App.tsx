cd36d4d7524223f92b1ed341a27c4da7f9bb451a
santosfk <santosguilhermefk@gmail.com>
2022-03-06 11:20:45 -0300

    add datefilter

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import { categories } from "./data/categories";
 import { items } from "./data/items";
 import { Category } from "./types/Category";
 import { Item } from "./types/Item";
+import { getCurrentMonth } from "./helpers/dateFilter.";
 function App() {
   const [list, setList] = useState(items);
-  const [currentMonth, setCurrentMonth] = useState();
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
   return (
     <style.Container>
       <style.Header>
         <style.Title>Personal Finances</style.Title>
       </style.Header>
-      <style.Body>..</style.Body>
+      <style.Body></style.Body>
     </style.Container>
   );
 }
--- src/helpers/dateFilter..ts
+++ src/helpers/dateFilter..ts
@@ -1,4 +1,4 @@
 export const getCurrentMonth = () => {
   let now = new Date();
-  return `${now.getFullYear} - ${now.getMonth}`;
+  return `${now.getFullYear()} - ${now.getMonth() + 1}`;
 };
