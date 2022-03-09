1d5b62fea939811ba820d97d48b0dd203e99cf0b
santosfk <santosguilhermefk@gmail.com>
2022-03-09 15:01:44 -0300

    adding date formatting

--- src/components/TableItems/index.tsx
+++ src/components/TableItems/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Item } from "../../types/Item";
+import { formatDate } from "../../helpers/dateFilter.";
 type Props = {
   item: Item;
 };
 function TableItems({ item }: Props) {
   return (
     <tr>
+      <td>{formatDate(item.date)}</td>
+      <td>{item.category}</td>
       <td>{item.title}</td>
+      <td>{item.value}</td>
     </tr>
   );
 }
