2adcbfe9900ca51456d84b5c3f26176df6986431
santosfk <santosguilhermefk@gmail.com>
2022-03-24 16:13:48 -0300

    implementing authentication through google

--- src/Pages/Signup/index.tsx
+++ src/Pages/Signup/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import * as style from "./style";
+import { firebase, auth } from "../../services/firebase";
 
 export default function Signup() {
+  const LoginInit = async () => {
+    const provider = new auth.GoogleAuthProvider();
+    const result = await auth.signInWithPopup(auth.getAuth(), provider);
+    console.log(result);
+  };
   return (
     <>
       <div>Signup</div>
-      <button>Logar</button>
+      <button onClick={LoginInit}>Logar</button>
     </>
   );
 }
