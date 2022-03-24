286002b305bf39539d3d4af4454f9db925890d40
santosfk <santosguilhermefk@gmail.com>
2022-03-24 16:32:47 -0300

    sending user to home page after login

--- src/Pages/Signup/index.tsx
+++ src/Pages/Signup/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import * as style from "./style";
 import { firebase, auth } from "../../services/firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const navigate = useNavigate();
   const LoginInit = async () => {
     const provider = new auth.GoogleAuthProvider();
     const result = await auth.signInWithPopup(auth.getAuth(), provider);
-    console.log(result);
+    result && navigate("home");
   };
   return (
     <>
