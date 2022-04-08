3577110859093fb8d4a07a93136030d418d999e7
santosfk <santosguilhermefk@gmail.com>
2022-04-08 15:16:33 -0300

    feat : remove signup screen

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home/Home";
-import Signup from "../Pages/Signup";
 
 export default function AllRoutes() {
   return (
     <Routes>
-      <Route path="" element={<Signup />} />
-      <Route path="home" element={<Home />} />
+      <Route path="" element={<Home />} />
     </Routes>
   );
 }
